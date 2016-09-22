a0479b1514b628242c0fb430a81a73ddc3f4396a
Daniel Ordonez <da.ordonez11@uniandes.edu.co>
2016-09-21 19:19:20 -0500

    Iteración 1 - TDD 2 Numero de elementos string de 1 número (Rojo)

--- test_processor.py
+++ test_processor.py
@@ -7,3 +7,4 @@ __author__ = 'Daniel Ordonez'
 class TestProcessor(TestCase):
     def test_process(self):
         self.assertEqual([0], Processor().process(""), "Numero de elementos cadena vacia")
+        self.assertEqual([1], Processor().process("1"), "Numero de elementos cadena vacia")
