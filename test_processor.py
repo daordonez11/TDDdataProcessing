9a1165dc258e6694281f4d51987be3dcab947c63
Daniel Ordonez <da.ordonez11@uniandes.edu.co>
2016-09-21 19:50:30 -0500

    Iteración 2 - TDD 3 Numero de elementos string 2 numeros (Verde)

--- test_processor.py
+++ test_processor.py
@@ -14,5 +14,5 @@ class TestProcessor(TestCase):
     def test_process2(self):
         self.assertEqual([0, 0], Processor().processMin(""), "Numero de elementos y min cadena vacia")
         self.assertEqual([1, 0], Processor().processMin("0"), "Numero de elementos y min cadena 1 numero")
-        self.assertEqual([2, 1], Processor().processMin("1,2"), "Numero de elementos y min cadena 2 numeros")
+        self.assertEqual([2, 0], Processor().processMin("1,2"), "Numero de elementos y min cadena 2 numeros")
         # self.assertEqual([4], Processor().process("0,1,2,3"), "Numero de elementos y min cadena N numeros")
